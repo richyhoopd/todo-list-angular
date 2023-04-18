265bd36015ede64505e729abc3d074b44427a06c
Ricardo Martinez Murillo <ricardo.martinez.murillo@ibm.com>
2023-04-18 16:04:37 -0600

    lo primero de lo primero, terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  tareas : Todo[] = [];
+  nuevaTarea : string;
+
+  guardarTarea() {
+    if(this.nuevaTarea){
+      let tarea = new Todo();
+      tarea.name = this.nuevaTarea;
+      tarea.completed = true
+      this.tareas.push(tarea)
+      this.nuevaTarea = '';
+    } else {
+      alert('por favor ingresa una tarea')
+    }
+  }
+
+  done(id:number) {
+    this.tareas[id].completed = !this.tareas[id].completed;
+  }
+
+  eliminar(id:number) {
+    this.tareas = this.tareas.filter((v,i)=> i !== id);
+  }
 }
+
